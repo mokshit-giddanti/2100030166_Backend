af1b8d3ce4543d289f10d0ea2b3c1eadb5fab82e
mokshit giddanti <mokshitlrao2016@gmail.com>
2024-04-29 18:58:31 +0530

    done

---
+++ join.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+# Connect to the database
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password",
+    database="mydb"
+)
+
+# Create a cursor object
+cursor = mydb.cursor()
+
+# Query using JOIN
+query = """
+SELECT l.location_id, l.street_address, l.city, l.state_province, c.country_name
+FROM locations l
+JOIN countries c ON l.country_id = c.country_id
+WHERE c.country_name = 'Canada'
+"""
+
+# Execute the query
+cursor.execute(query)
+
+# Fetch all the results
+results = cursor.fetchall()
+
+# Print the results
+for result in results:
+    print(result)
+
+# Close the cursor and connection
+cursor.close()
+mydb.close()
